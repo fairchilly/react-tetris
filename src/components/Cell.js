e613100cbaa92589be541566d4213aef060d0af7
sf88 <sf88@queensu.ca>
2019-08-15 15:28:53 -0400

    added more styled components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOES } from '../tetrominoes';
 
-const Cell = ({ type }) => <div>cell</div>;
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOES[type].color} />
+);
 
 export default Cell;
